b32e5ee88c3ef45a893d882b5a8252bef041f11f
Anvi Alex <epononanvi@gmail.com>
2025-01-02 12:27:41 -0600

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sherlock",
-    version="1.0.0",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
