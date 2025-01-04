956502232757d42de6a215b0a0b32566b97ea62c
Anvi Alex <epononanvi@gmail.com>
2025-01-04 13:21:40 -0600

    fix imports

--- src/sherlock/__init__.py
+++ src/sherlock/__init__.py
@@ -1,5 +1,5 @@
-from .core import hello, generate, check_requirements
+from .core import generate, check_requirements
 
 __version__ = "0.1.0"
 
-__all__ = ['hello', 'generate', 'check_requirements']
+__all__ = ['generate', 'check_requirements']
