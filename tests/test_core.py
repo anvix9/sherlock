49d6be2a1adf2eff72fd6cd4f16fe0d33599eeb5
Anvi Alex <epononanvi@gmail.com>
2025-01-03 22:05:16 -0600

    add init script in src

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,9 @@
 # tests/test_core.py
-from src.sherlock.core import hello
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from sherlock.core import hello
 
 def test_hello():
     assert hello() == "Hello from Sherlock!"
