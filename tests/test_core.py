f077be98333e0ccbf2f3016302c400d71dc6bd00
Anvi Alex <epononanvi@gmail.com>
2025-01-03 22:14:23 -0600

    fix path issue in container

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,6 @@
 # tests/test_core.py
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from sherlock.core import hello
+from core import hello
 
 def test_hello():
     assert hello() == "Hello from Sherlock!"
